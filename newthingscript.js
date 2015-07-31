c1e194a9149d4118748071e378c9e2a21210f7d8
schmooblidon <will_beats@hotmail.co.uk>
2015-07-31 22:02:30 +0100

    functioningcontrollerdisplay

--- newthingscript.js
+++ newthingscript.js
@@ -7,6 +7,7 @@
 
 /*timelineframelength = 10;
 */
+  controllertext = '<img id="i-lb" src="assets/condisplay/lbutton.png"><img id="i-rb" src="assets/condisplay/rbutton.png"><img id="i-ll" src="assets/condisplay/ltext.png"><img id="i-rr" src="assets/condisplay/rtext.png"><img id="i-zb" src="assets/condisplay/zbutton.png"><img id="i-zz" src="assets/condisplay/ztext.png"><img id="base" src="assets/condisplay/base.png"><img id="i-aa" src="assets/condisplay/a.png"><img id="i-bb" src="assets/condisplay/b.png"><img id="i-xx" src="assets/condisplay/x.png"><img id="i-yy" src="assets/condisplay/y.png"><img id="i-ss" src="assets/condisplay/start.png"><img id="i-du" class="dpad" src="assets/condisplay/dpup.png"><img id="i-dr" class="dpad" src="assets/condisplay/dpright.png"><img id="i-dd" class="dpad" src="assets/condisplay/dpdown.png"><img id="i-dl" class="dpad" src="assets/condisplay/dpleft.png"><img id="i-cs" src="assets/condisplay/cstick.png"><img id="i-ls" src="assets/condisplay/lstick.png">';
 playing = false;
 timelines = {};
 game = "";
@@ -17,6 +18,8 @@ charid = [];
 noofbuttons = 0;
 noofframes = 0;
 buttons = [];
+speed = 1;
+loop = true;
 /*
 for (i=1;i<=noofbuttons;i++){
   timelines["tl"+i] = [];
@@ -29,18 +32,42 @@ framescript = "1003sd1222222122bb1222220122xy0000122222";
 
 var tframeclickstates = function(){
   $("#timelinebigcontainer").on('click','.tframe',function() {
+
     var $t = $(this);
+    var id = $t.attr("id");
+    if (id[2] === "f"){
+      var but = parseInt(id.substr(1,1));
+      if (id.length === 4){
+        var frame = parseInt(id.substr(3,1));
+      }
+      else {
+        var frame = parseInt(id.substr(3,2));
+      }
+    }
+    else {
+      but = parseInt(id.substr(1,2));
+      if (id.length === 5){
+        var frame = parseInt(id.substr(4,1));
+      }
+      else {
+        var frame = parseInt(id.substr(4,2));
+      }
+    }
+
     if ($t.hasClass("type0")){
       $t.removeClass("type0");
       $t.addClass("type1");
+      timelines["tl"+but][frame-1] = 1;
     }
     else if ($t.hasClass("type1")){
       $t.removeClass("type1");
       $t.addClass("type2");
+      timelines["tl"+but][frame-1] = 2;
     }
     else if ($t.hasClass("type2")){
       $t.removeClass("type2");
       $t.addClass("type0");
+      timelines["tl"+but][frame-1] = 0;
     }
   });
 }
@@ -137,11 +164,13 @@ var createnewtimeline = function(ngame,nchar,nname,nnoofbuttons,nnoofframes){
 
   for (i=1;i<=noofbuttons;i++){
     timelines["tl"+i] = [];
+    buttons[i-1] = "0";
     for (j=1;j<=noofframes;j++){
       timelines["tl"+i][j-1] = 0;
     }
   }
-  $("#timelinebigcontainer").width(105+(72*noofframes)).append('<div id="tline" style="height:'+(72*(noofbuttons+1))+'px"></div><div id="timelinescontainer"></div>');
+
+  $("#timelinebigcontainer").width(110+(72*noofframes)).append('<div id="controllerdisplay">'+controllertext+'</div><div id="tline" style="height:'+(72*(noofbuttons+1))+'px"></div><div id="timelinescontainer"></div>');
 
   for (i=1;i<=noofbuttons;i++){
     $("#timelinescontainer").append('<div class="timeline" id="tl'+i+'"><div class="tkey"></div></div>');
@@ -151,7 +180,7 @@ var createnewtimeline = function(ngame,nchar,nname,nnoofbuttons,nnoofframes){
     }
   }
 
-  $("#timelinescontainer").width(102+(72*noofframes)).append('<div class="timeline" id="playline"><div id="playkey" class="playmode"></div></div>');
+  $("#timelinescontainer").width(106+(72*noofframes)).append('<div class="timeline" id="playline"><div id="playkey" class="playmode"></div></div><div class="timeline" id="extracontrols"><div id="playcontrols"><div id="speed1" class="playbutton playbuttonhighlight2"><p>1x</p></div><div id="speed05" class="playbutton"><p>0.5</p></div><div id="speed025" class="playbutton"><p>0.25</p></div><div id="loop" class="playbutton" style="background-image:url(assets/buttons/loop.png)"></div></div></div>');
   for (j=1;j<=noofframes;j++){
       $("#playline").append('<div class="tnum" id="num'+j+'"><p>'+j+'</p></div>');
   }
@@ -227,18 +256,53 @@ var buildtimeline = function(framescript){
   }
 
 var playtimeline = function(){
-
   var $tl = $("#tline");
   $tl.css("left","103px");
   if (playing){
-    $tl.animate({left : 103+(noofframes * 72)},16*noofframes, function() {
-        playtimeline();
+    $tl.animate({left : 103+(noofframes * 72)},16*noofframes*speed, "linear",function() {
+        if (loop){
+          playtimeline();
+        }
+        else {
+          $tl.css("left","103px");
+          $("#playkey").removeClass("pausemode").addClass("playmode");
+        }
       }
     );
+    (function buttondisplay (j) {
+      setTimeout(function () {
+        for (i=1;i<=noofbuttons;i++){
+          if (timelines["tl"+i][noofframes-j]){
+            $("#i-"+buttons[i-1]).fadeTo(1,1);
+            if (buttons[i-1] === "ll"){
+              $("#i-lb").css("top","123px");
+            }
+            else if (buttons[i-1] === "rr"){
+              $("#i-rb").css("top","123px");
+            }
+            else if (buttons[i-1] === "zz"){
+              $("#i-zb").css("top","133px");
+            }
+          }
+          else {
+            $("#i-"+buttons[i-1]).fadeTo(1,0.35);
+            if (buttons[i-1] === "ll"){
+              $("#i-lb").css("top","108px");
+            }
+            else if (buttons[i-1] === "rr"){
+              $("#i-rb").css("top","108px");
+            }
+            else if (buttons[i-1] === "zz"){
+              $("#i-zb").css("top","123px");
+            }
+
+          }
+        }
+        if (--j) buttondisplay(j);
+      }, 16*speed)
+    })(noofframes);
   }
 }
-
-
 
 $(document).ready(function(){
   $("#gamedrop").change(function() {
@@ -301,7 +365,7 @@ $(document).ready(function(){
     else {
       $(".tkey").removeClass("dropdowned");
       $("#buttondropdown").remove();
-      $(this).append('<div id="buttondropdown"><div class="buttonselect" id="sd" style="background-image:url(assets/buttons/sd.png)"></div><div class="buttonselect" id="bb" style="background-image:url(assets/buttons/bb.png)"></div><div class="buttonselect" id="xy" style="background-image:url(assets/buttons/xy.png)"></div></div>');
+      $(this).append('<div id="buttondropdown"><div class="buttonselect" id="ls" style="background-image:url(assets/buttons/ls.png)"></div><div class="buttonselect" id="cs" style="background-image:url(assets/buttons/cs.png)"></div><div class="buttonselect" id="aa" style="background-image:url(assets/buttons/aa.png)"></div><div class="buttonselect" id="bb" style="background-image:url(assets/buttons/bb.png)"></div><div class="buttonselect" id="xx" style="background-image:url(assets/buttons/xx.png)"></div><div class="buttonselect" id="yy" style="background-image:url(assets/buttons/yy.png)"></div><div class="buttonselect" id="ss" style="background-image:url(assets/buttons/ss.png)"></div><div class="buttonselect" id="zz" style="background-image:url(assets/buttons/zz.png)"></div><div class="buttonselect" id="ll" style="background-image:url(assets/buttons/ll.png)"></div><div class="buttonselect" id="rr" style="background-image:url(assets/buttons/rr.png)"></div>');
       $(this).addClass("dropdowned");
     }
   });
@@ -333,13 +397,29 @@ $(document).ready(function(){
     }
   }, "#playkey");
 
+  $("#timelinebigcontainer").on({
+    mouseenter: function () {
+        $(this).addClass("playbuttonhighlight");
+    },
+    mouseleave: function () {
+        $(this).removeClass("playbuttonhighlight");
+    }
+  }, ".playbutton");
 
 
   $("#timelinebigcontainer").on('click','.buttonselect',function() {
+    $(this).closest(".tkey").removeClass("aa bb ss xx yy ls cs zz");
     var but = $(this).attr("id");
-    $(this).closest(".tkey").css("background-image","url(assets/buttons/"+but+".png)");
+    var num = $(this).closest(".timeline").attr("id");
+    if (num.length === 4){
+      num = parseInt(num.substr(2,2));
+    }
+    else {
+      num = parseInt(num.substr(2,1));
+    }
+    buttons[num-1] = but;
+    $(this).closest(".tkey").addClass(but).css("background-image","url(assets/buttons/"+but+".png)");
     $(".tkey").removeClass("tkeyhighlight");
-
   });
 
   $("#timelinebigcontainer").on('click','.playmode',function() {
@@ -351,6 +431,32 @@ $(document).ready(function(){
   $("#timelinebigcontainer").on('click','.pausemode',function() {
     $("#playkey").removeClass("pausemode").addClass("playmode");
     playing = false;
+  });
+
+  $("#timelinebigcontainer").on('click','#speed1',function() {
+    $(".playbutton").removeClass("playbuttonhighlight2");
+    $(this).addClass("playbuttonhighlight2");
+    speed = 1;
+  });
+  $("#timelinebigcontainer").on('click','#speed05',function() {
+    $(".playbutton").removeClass("playbuttonhighlight2");
+    $(this).addClass("playbuttonhighlight2");
+    speed = 2;
+  });
+  $("#timelinebigcontainer").on('click','#speed025',function() {
+    $(".playbutton").removeClass("playbuttonhighlight2");
+    $(this).addClass("playbuttonhighlight2");
+    speed = 4;
+  });
+  $("#timelinebigcontainer").on('click','#loop',function() {
+    if (loop){
+      loop = false;
+      $(this).css("background-image","url(assets/buttons/noloop.png)");
+    }
+    else {
+      loop = true;
+      $(this).css("background-image","url(assets/buttons/loop.png)");
+    }
   });
 
 
