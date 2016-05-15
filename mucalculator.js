cea027cf9eed6870ae11b65be98b79ea9be30140
schmooblidon <will_beats@hotmail.co.uk>
2016-05-15 21:50:26 +0100

    bug fix

--- mucalculator.js
+++ mucalculator.js
@@ -34,58 +34,59 @@ function deleteNonNumbers(text,allowNegative,allowPoint,allowZeros){
 
 
 function calculatePercents(){
+  starter,ocp1,ocp2,ycp1,ycp2
   // B O 3
-  $("#bo3set1").empty().append(Math.round(twoZero()*100));
-  $("#bo3set2").empty().append(Math.round(twoOne()*100));
-  $("#bo3set3").empty().append(Math.round(oneTwo()*100));
-  $("#bo3set4").empty().append(Math.round(zeroTwo()*100));
-  $("#bo3win").empty().append(Math.round(win3()*100));
-  $("#bo3lose").empty().append(Math.round(lose3()*100));
+  $("#bo3set1").empty().append(Math.round(twoZero(starter,ocp1,ocp2,ycp1,ycp2)*100));
+  $("#bo3set2").empty().append(Math.round(twoOne(starter,ocp1,ocp2,ycp1,ycp2)*100));
+  $("#bo3set3").empty().append(Math.round(oneTwo(starter,ocp1,ocp2,ycp1,ycp2)*100));
+  $("#bo3set4").empty().append(Math.round(zeroTwo(starter,ocp1,ocp2,ycp1,ycp2)*100));
+  $("#bo3win").empty().append(Math.round(win3(starter,ocp1,ocp2,ycp1,ycp2)*100));
+  $("#bo3lose").empty().append(Math.round(lose3(starter,ocp1,ocp2,ycp1,ycp2)*100));
 
   // B O 5
   if ((yRepick == true) && (oRepick == true) && (ycp1 >= ycp2) && (ocp1 <= ocp2)){
     console.log("t1");
-    $("#bo5set1").empty().append(Math.round(rThreeZero()*100));
-    $("#bo5set2").empty().append(Math.round(rThreeOne()*100));
-    $("#bo5set3").empty().append(Math.round(rThreeTwo()*100));
-    $("#bo5set4").empty().append(Math.round(rTwoThree()*100));
-    $("#bo5set5").empty().append(Math.round(rOneThree()*100));
-    $("#bo5set6").empty().append(Math.round(rZeroThree()*100));
-    $("#bo5win").empty().append(Math.round(rWin5()*100));
-    $("#bo5lose").empty().append(Math.round(rLose5()*100));
+    $("#bo5set1").empty().append(Math.round(rThreeZero(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set2").empty().append(Math.round(rThreeOne(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set3").empty().append(Math.round(rThreeTwo(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set4").empty().append(Math.round(rTwoThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set5").empty().append(Math.round(rOneThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set6").empty().append(Math.round(rZeroThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5win").empty().append(Math.round(rWin5(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5lose").empty().append(Math.round(rLose5(starter,ocp1,ocp2,ycp1,ycp2)*100));
   }
   else if (((yRepick == true) && (oRepick == false) && (ycp1 > ycp2)) || ((yRepick == true) && (oRepick == true) && (ycp1 > ycp2) && (ocp1 > ocp2))){
     console.log("t2");
-    $("#bo5set1").empty().append(Math.round(yRThreeZero()*100));
-    $("#bo5set2").empty().append(Math.round(yRThreeOne()*100));
-    $("#bo5set3").empty().append(Math.round(yRThreeTwo()*100));
-    $("#bo5set4").empty().append(Math.round(yRTwoThree()*100));
-    $("#bo5set5").empty().append(Math.round(yROneThree()*100));
-    $("#bo5set6").empty().append(Math.round(yRZeroThree()*100));
-    $("#bo5win").empty().append(Math.round(yRWin5()*100));
-    $("#bo5lose").empty().append(Math.round(yRLose5()*100));
+    $("#bo5set1").empty().append(Math.round(yRThreeZero(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set2").empty().append(Math.round(yRThreeOne(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set3").empty().append(Math.round(yRThreeTwo(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set4").empty().append(Math.round(yRTwoThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set5").empty().append(Math.round(yROneThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set6").empty().append(Math.round(yRZeroThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5win").empty().append(Math.round(yRWin5(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5lose").empty().append(Math.round(yRLose5(starter,ocp1,ocp2,ycp1,ycp2)*100));
   }
   else if (((oRepick == true) && (yRepick == false) && (ocp1 < ocp2)) || ((oRepick == true) && (yRepick == true) && (ocp1<ocp2) && (ycp1<ycp2))){
     console.log("t3");
-    $("#bo5set1").empty().append(Math.round(eRThreeZero()*100));
-    $("#bo5set2").empty().append(Math.round(eRThreeOne()*100));
-    $("#bo5set3").empty().append(Math.round(eRThreeTwo()*100));
-    $("#bo5set4").empty().append(Math.round(eRTwoThree()*100));
-    $("#bo5set5").empty().append(Math.round(eROneThree()*100));
-    $("#bo5set6").empty().append(Math.round(eRZeroThree()*100));
-    $("#bo5win").empty().append(Math.round(eRWin5()*100));
-    $("#bo5lose").empty().append(Math.round(eRLose5()*100));
+    $("#bo5set1").empty().append(Math.round(eRThreeZero(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set2").empty().append(Math.round(eRThreeOne(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set3").empty().append(Math.round(eRThreeTwo(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set4").empty().append(Math.round(eRTwoThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set5").empty().append(Math.round(eROneThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set6").empty().append(Math.round(eRZeroThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5win").empty().append(Math.round(eRWin5(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5lose").empty().append(Math.round(eRLose5(starter,ocp1,ocp2,ycp1,ycp2)*100));
   }
   else if (((yRepick == false) && (oRepick == false)) || ((ycp1 <= ycp2) && (ocp1 >= ocp2))){
     console.log("t4");
-    $("#bo5set1").empty().append(Math.round(threeZero()*100));
-    $("#bo5set2").empty().append(Math.round(threeOne()*100));
-    $("#bo5set3").empty().append(Math.round(threeTwo()*100));
-    $("#bo5set4").empty().append(Math.round(twoThree()*100));
-    $("#bo5set5").empty().append(Math.round(oneThree()*100));
-    $("#bo5set6").empty().append(Math.round(zeroThree()*100));
-    $("#bo5win").empty().append(Math.round(win5()*100));
-    $("#bo5lose").empty().append(Math.round(lose5()*100));
+    $("#bo5set1").empty().append(Math.round(threeZero(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set2").empty().append(Math.round(threeOne(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set3").empty().append(Math.round(threeTwo(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set4").empty().append(Math.round(twoThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set5").empty().append(Math.round(oneThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5set6").empty().append(Math.round(zeroThree(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5win").empty().append(Math.round(win5(starter,ocp1,ocp2,ycp1,ycp2)*100));
+    $("#bo5lose").empty().append(Math.round(lose5(starter,ocp1,ocp2,ycp1,ycp2)*100));
   }
 }
 
--- mucalculatormaths.js
+++ mucalculatormaths.js
@@ -7,229 +7,229 @@ ocp2 = 0.5;
 ycp1 = 0.5;
 ycp2 = 0.5;
 
-function twoZero(){
-  if(ocp1<ocp2){
-    ocp1=ocp2;
+function twoZero(start,o1,o2,y1,y2){
+  if(o1<o2){
+    o1=o2;
   }
-  if(ycp1>ycp2){
-    ycp1=ycp2;
+  if(y1>y2){
+    y1=y2;
   }
-  var twoZerov = (starter)*(ocp1);
+  var twoZerov = (start)*(o1);
   return twoZerov;
 }
 
-function twoOne(){
-  if(ocp1<ocp2){
-    ocp1=ocp2;
+function twoOne(start,o1,o2,y1,y2){
+  if(o1<o2){
+    o1=o2;
   }
-  if(ycp1>ycp2){
-    ycp1=ycp2;
+  if(y1>y2){
+    y1=y2;
   }
-  twoOnev = (starter)*(1-ocp1)*(ycp1)+(1-starter)*(ycp1)*(ocp1);
+  twoOnev = (start)*(1-o1)*(y1)+(1-start)*(y1)*(o1);
   return twoOnev;
 }
 
-function oneTwo(){
-  if(ocp1<ocp2){
-    ocp1=ocp2;
+function oneTwo(start,o1,o2,y1,y2){
+  if(o1<o2){
+    o1=o2;
   }
-  if(ycp1>ycp2){
-    ycp1=ycp2;
+  if(y1>y2){
+    y1=y2;
   }
-  oneTwov = (starter)*(1-ocp1)*(1-ycp1)+(1-starter)*(ycp1)*(1-ocp1);
+  oneTwov = (start)*(1-o1)*(1-y1)+(1-start)*(y1)*(1-o1);
   return oneTwov;
 }
 
-function zeroTwo(){
-  if(ocp1<ocp2){
-    ocp1=ocp2;
+function zeroTwo(start,o1,o2,y1,y2){
+  if(o1<o2){
+    o1=o2;
   }
-  if(ycp1>ycp2){
-    ycp1=ycp2;
+  if(y1>y2){
+    y1=y2;
   }
-  zeroTwov = (1-starter)*(1-ycp1);
+  zeroTwov = (1-start)*(1-y1);
   return zeroTwov;
 }
 
 
-function rThreeZero(){
-  rThreeZerov = (starter)*(ocp1)*(ocp1);
+function rThreeZero(start,o1,o2,y1,y2){
+  rThreeZerov = (start)*(o1)*(o1);
   return rThreeZerov;
 }
 
 
-function rThreeOne(){
-  rThreeOnev = (starter)*(1-ocp1)*(ycp1)*(ocp2)+(starter)*(ocp1)*(1-ocp1)*(ycp1)+(1-starter)*(ycp1)*(ocp1)*(ocp1);
+function rThreeOne(start,o1,o2,y1,y2){
+  rThreeOnev = (start)*(1-o1)*(y1)*(o2)+(start)*(o1)*(1-o1)*(y1)+(1-start)*(y1)*(o1)*(o1);
   return rThreeOnev;
 }
 
-function rThreeTwo(){
-  rThreeTwov = (starter)*(ocp1)*(1-ocp1)*(1-ycp1)*(ycp1)+(starter)*(1-ocp1)*(1-ycp1)*(ycp1)*(ocp2) +(1-starter)*(1-ycp1)*(ycp1)*(ocp1)*(ocp1)+(1-starter)*(ycp1)*(1-ocp1)*(ycp2)*(ocp2)+(1-starter)*(ycp1)*(ocp1)*(1-ocp1)*(ycp2)+(starter)*(1-ocp1)*(ycp1)*(1-ocp2)*(ycp2);
+function rThreeTwo(start,o1,o2,y1,y2){
+  rThreeTwov = (start)*(o1)*(1-o1)*(1-y1)*(y1)+(start)*(1-o1)*(1-y1)*(y1)*(o2) +(1-start)*(1-y1)*(y1)*(o1)*(o1)+(1-start)*(y1)*(1-o1)*(y2)*(o2)+(1-start)*(y1)*(o1)*(1-o1)*(y2)+(start)*(1-o1)*(y1)*(1-o2)*(y2);
   return rThreeTwov
 }
 
-function rTwoThree(){
-  rTwoThreev=(1-starter)*(1-ycp1)*(ycp1)*(ocp1)*(1-ocp1)+(1-starter)*(ycp1)*(ocp1)*(1-ocp1)*(1-ycp2)+(starter)*(ocp1)*(1-ocp1)*(1-ycp1)*(1-ycp1)+(starter)*(1-ocp1)*(ycp1)*(1-ocp2)*(1-ycp2)+(starter)*(1-ocp1)*(1-ycp1)*(ycp1)*(1-ocp2)+(1-starter)*(ycp1)*(1-ocp1)*(ycp2)*(1-ocp2);
+function rTwoThree(start,o1,o2,y1,y2){
+  rTwoThreev=(1-start)*(1-y1)*(y1)*(o1)*(1-o1)+(1-start)*(y1)*(o1)*(1-o1)*(1-y2)+(start)*(o1)*(1-o1)*(1-y1)*(1-y1)+(start)*(1-o1)*(y1)*(1-o2)*(1-y2)+(start)*(1-o1)*(1-y1)*(y1)*(1-o2)+(1-start)*(y1)*(1-o1)*(y2)*(1-o2);
   return rTwoThreev;
 }
 
-function rOneThree(){
-  rOneThreev=(1-starter)*(ycp1)*(1-ocp1)*(1-ycp2)+(1-starter)*(1-ycp1)*(ycp1)*(1-ocp1)+(starter)*(1-ocp1)*(1-ycp1)*(1-ycp1);
+function rOneThree(start,o1,o2,y1,y2){
+  rOneThreev=(1-start)*(y1)*(1-o1)*(1-y2)+(1-start)*(1-y1)*(y1)*(1-o1)+(start)*(1-o1)*(1-y1)*(1-y1);
   return rOneThreev;
 }
 
-function rZeroThree(){
-  rZeroThreev=(1-starter)*(1-ycp1)*(1-ycp1);
+function rZeroThree(start,o1,o2,y1,y2){
+  rZeroThreev=(1-start)*(1-y1)*(1-y1);
   return rZeroThreev;
 }
 
-function win3(){
-  winChance = (twoZero())+(twoOne());
+function win3(start,o1,o2,y1,y2){
+  winChance = (twoZero(start,o1,o2,y1,y2))+(twoOne(start,o1,o2,y1,y2));
   return winChance;
 }
 
-function lose3(){
-  loseChance=(oneTwo())+(zeroTwo());
+function lose3(start,o1,o2,y1,y2){
+  loseChance=(oneTwo(start,o1,o2,y1,y2))+(zeroTwo(start,o1,o2,y1,y2));
   return loseChance;
 }
 
-function rWin5(){
-  winChance=(rThreeZero()+rThreeOne()+rThreeTwo());
+function rWin5(start,o1,o2,y1,y2){
+  winChance=(rThreeZero(start,o1,o2,y1,y2)+rThreeOne(start,o1,o2,y1,y2)+rThreeTwo(start,o1,o2,y1,y2));
   return winChance;
 }
 
-function rLose5(){
-  loseChance=rTwoThree()+rOneThree()+rZeroThree();
+function rLose5(start,o1,o2,y1,y2){
+  loseChance=rTwoThree(start,o1,o2,y1,y2)+rOneThree(start,o1,o2,y1,y2)+rZeroThree(start,o1,o2,y1,y2);
   return loseChance;
 }
 
-function threeZero(){
-  threeZerov=(starter)*(ocp1)*(ocp2);
+function threeZero(start,o1,o2,y1,y2){
+  threeZerov=(start)*(o1)*(o2);
   return threeZerov;
 }
 
 
-function threeOne(){
-  threeOnev=(starter)*(1-ocp1)*(ycp1)*(ocp2)+(starter)*(ocp1)*(1-ocp2)*(ycp1)+(1-starter)*(ycp1)*(ocp1)*(ocp2);
+function threeOne(start,o1,o2,y1,y2){
+  threeOnev=(start)*(1-o1)*(y1)*(o2)+(start)*(o1)*(1-o2)*(y1)+(1-start)*(y1)*(o1)*(o2);
   return threeOnev;
 }
 
-function threeTwo(){
-  threeTwov=(starter)*(ocp1)*(1-ocp2)*(1-ycp1)*(ycp2)+(starter)*(1-ocp1)*(1-ycp1)*(ycp2)*(ocp2)+(1-starter)*(1-ycp1)*(ycp2)*(ocp1)*(ocp2)+(1-starter)*(ycp1)*(1-ocp1)*(ycp2)*(ocp2)+(1-starter)*(ycp1)*(ocp1)*(1-ocp2)*(ycp2)+(starter)*(1-ocp1)*(ycp1)*(1-ocp2)*(ycp2);
+function threeTwo(start,o1,o2,y1,y2){
+  threeTwov=(start)*(o1)*(1-o2)*(1-y1)*(y2)+(start)*(1-o1)*(1-y1)*(y2)*(o2)+(1-start)*(1-y1)*(y2)*(o1)*(o2)+(1-start)*(y1)*(1-o1)*(y2)*(o2)+(1-start)*(y1)*(o1)*(1-o2)*(y2)+(start)*(1-o1)*(y1)*(1-o2)*(y2);
   return threeTwov;
 }
 
-function twoThree(){
-  twoThreev=(1-starter)*(1-ycp1)*(ycp2)*(ocp1)*(1-ocp2)+(1-starter)*(ycp1)*(ocp1)*(1-ocp2)*(1-ycp2)+(starter)*(ocp1)*(1-ocp2)*(1-ycp1)*(1-ycp2)+(starter)*(1-ocp1)*(ycp1)*(1-ocp2)*(1-ycp2)+(starter)*(1-ocp1)*(1-ycp1)*(ycp2)*(1-ocp2)+(1-starter)*(ycp1)*(1-ocp1)*(ycp2)*(1-ocp2);
+function twoThree(start,o1,o2,y1,y2){
+  twoThreev=(1-start)*(1-y1)*(y2)*(o1)*(1-o2)+(1-start)*(y1)*(o1)*(1-o2)*(1-y2)+(start)*(o1)*(1-o2)*(1-y1)*(1-y2)+(start)*(1-o1)*(y1)*(1-o2)*(1-y2)+(start)*(1-o1)*(1-y1)*(y2)*(1-o2)+(1-start)*(y1)*(1-o1)*(y2)*(1-o2);
   return twoThreev;
 }
 
-function oneThree(){
-  oneThreev=(1-starter)*(ycp1)*(1-ocp1)*(1-ycp2)+(1-starter)*(1-ycp1)*(ycp2)*(1-ocp1)+(starter)*(1-ocp1)*(1-ycp1)*(1-ycp2);
+function oneThree(start,o1,o2,y1,y2){
+  oneThreev=(1-start)*(y1)*(1-o1)*(1-y2)+(1-start)*(1-y1)*(y2)*(1-o1)+(start)*(1-o1)*(1-y1)*(1-y2);
   return oneThreev;
 }
 
-function zeroThree(){
-  zeroThreev=(1-starter)*(1-ycp1)*(1-ycp2);
+function zeroThree(start,o1,o2,y1,y2){
+  zeroThreev=(1-start)*(1-y1)*(1-y2);
   return zeroThreev;
 }
 
-function win3(){
-  winChance=(twoZero())+(twoOne());
+function win3(start,o1,o2,y1,y2){
+  winChance=(twoZero(start,o1,o2,y1,y2))+(twoOne(start,o1,o2,y1,y2));
   return winChance;
 }
 
-function lose3(){
-  loseChance=(oneTwo())+(zeroTwo());
+function lose3(start,o1,o2,y1,y2){
+  loseChance=(oneTwo(start,o1,o2,y1,y2))+(zeroTwo(start,o1,o2,y1,y2));
   return loseChance;
 }
 
-function win5(){
-  winChance=(threeZero()+threeOne()+threeTwo());
+function win5(start,o1,o2,y1,y2){
+  winChance=(threeZero(start,o1,o2,y1,y2)+threeOne(start,o1,o2,y1,y2)+threeTwo(start,o1,o2,y1,y2));
   return winChance;
 }
 
-function lose5(){
-  loseChance=twoThree()+oneThree()+zeroThree();
+function lose5(start,o1,o2,y1,y2){
+  loseChance=twoThree(start,o1,o2,y1,y2)+oneThree(start,o1,o2,y1,y2)+zeroThree(start,o1,o2,y1,y2);
   return loseChance;
 }
 
-function yRThreeZero(){
-  yRThreeZerov=(starter)*(ocp1)*(ocp2);
+function yRThreeZero(start,o1,o2,y1,y2){
+  yRThreeZerov=(start)*(o1)*(o2);
   return yRThreeZerov;
 }
 
-function yRThreeOne(){
-  yRThreeOnev=(starter)*(1-ocp1)*(ycp1)*(ocp2)+(starter)*(ocp1)*(1-ocp2)*(ycp1)+(1-starter)*(ycp1)*(ocp1)*(ocp2);
+function yRThreeOne(start,o1,o2,y1,y2){
+  yRThreeOnev=(start)*(1-o1)*(y1)*(o2)+(start)*(o1)*(1-o2)*(y1)+(1-start)*(y1)*(o1)*(o2);
   return YRThreeOnev;
 }
 
-function yRThreeTwo(){
-  yRThreeTwov=(starter)*(ocp1)*(1-ocp2)*(1-ycp1)*(ycp1)+(starter)*(1-ocp1)*(1-ycp1)*(ycp1)*(ocp2)+(1-starter)*(1-ycp1)*(ycp1)*(ocp1)*(ocp2)+(1-starter)*(ycp1)*(1-ocp1)*(ycp2)*(ocp2)+(1-starter)*(ycp1)*(ocp1)*(1-ocp2)*(ycp2)+(starter)*(1-ocp1)*(ycp1)*(1-ocp2)*(ycp2);
+function yRThreeTwo(start,o1,o2,y1,y2){
+  yRThreeTwov=(start)*(o1)*(1-o2)*(1-y1)*(y1)+(start)*(1-o1)*(1-y1)*(y1)*(o2)+(1-start)*(1-y1)*(y1)*(o1)*(o2)+(1-start)*(y1)*(1-o1)*(y2)*(o2)+(1-start)*(y1)*(o1)*(1-o2)*(y2)+(start)*(1-o1)*(y1)*(1-o2)*(y2);
   return yRThreeTwov;
 }
 
-function yRTwoThree(){
-  yRTwoThreev=(1-starter)*(1-ycp1)*(ycp1)*(ocp1)*(1-ocp2)+(1-starter)*(ycp1)*(ocp1)*(1-ocp2)*(1-ycp2)+(starter)*(ocp1)*(1-ocp2)*(1-ycp1)*(1-ycp1)+(starter)*(1-ocp1)*(ycp1)*(1-ocp2)*(1-ycp2)+(starter)*(1-ocp1)*(1-ycp1)*(ycp1)*(1-ocp2)+(1-starter)*(ycp1)*(1-ocp1)*(ycp2)*(1-ocp2);
+function yRTwoThree(start,o1,o2,y1,y2){
+  yRTwoThreev=(1-start)*(1-y1)*(y1)*(o1)*(1-o2)+(1-start)*(y1)*(o1)*(1-o2)*(1-y2)+(start)*(o1)*(1-o2)*(1-y1)*(1-y1)+(start)*(1-o1)*(y1)*(1-o2)*(1-y2)+(start)*(1-o1)*(1-y1)*(y1)*(1-o2)+(1-start)*(y1)*(1-o1)*(y2)*(1-o2);
   return yRTwoThreev;
 }
 
-function yROneThree(){
-  yROneThreev=(1-starter)*(ycp1)*(1-ocp1)*(1-ycp2)+(1-starter)*(1-ycp1)*(ycp1)*(1-ocp1)+(starter)*(1-ocp1)*(1-ycp1)*(1-ycp1);
+function yROneThree(start,o1,o2,y1,y2){
+  yROneThreev=(1-start)*(y1)*(1-o1)*(1-y2)+(1-start)*(1-y1)*(y1)*(1-o1)+(start)*(1-o1)*(1-y1)*(1-y1);
   return yROneThreev;
 }
 
-function yRZeroThree(){
-  yRZeroThreev=(1-starter)*(1-ycp1)*(1-ycp1);
+function yRZeroThree(start,o1,o2,y1,y2){
+  yRZeroThreev=(1-start)*(1-y1)*(1-y1);
   return yRZeroThreev;
 }
 
-function eRThreeZero(){
-  eRThreeZerov=(starter)*(ocp1)*(ocp1);
+function eRThreeZero(start,o1,o2,y1,y2){
+  eRThreeZerov=(start)*(o1)*(o1);
   return eRThreeZerov;
 }
 
-function eRThreeOne(){
-  eRThreeOnev=(starter)*(1-ocp1)*(ycp1)*(ocp2)+(starter)*(ocp1)*(1-ocp1)*(ycp1)+(1-starter)*(ycp1)*(ocp1)*(ocp1);
+function eRThreeOne(start,o1,o2,y1,y2){
+  eRThreeOnev=(start)*(1-o1)*(y1)*(o2)+(start)*(o1)*(1-o1)*(y1)+(1-start)*(y1)*(o1)*(o1);
   return eRThreeOnev;
 }
 
-function eRThreeTwo(){
-  eRThreeTwov=(starter)*(ocp1)*(1-ocp1)*(1-ycp1)*(ycp2)+(starter)*(1-ocp1)*(1-ycp1)*(ycp2)*(ocp2)+(1-starter)*(1-ycp1)*(ycp2)*(ocp1)*(ocp1)+(1-starter)*(ycp1)*(1-ocp1)*(ycp2)*(ocp2)+(1-starter)*(ycp1)*(ocp1)*(1-ocp1)*(ycp2)+(starter)*(1-ocp1)*(ycp1)*(1-ocp2)*(ycp2);
+function eRThreeTwo(start,o1,o2,y1,y2){
+  eRThreeTwov=(start)*(o1)*(1-o1)*(1-y1)*(y2)+(start)*(1-o1)*(1-y1)*(y2)*(o2)+(1-start)*(1-y1)*(y2)*(o1)*(o1)+(1-start)*(y1)*(1-o1)*(y2)*(o2)+(1-start)*(y1)*(o1)*(1-o1)*(y2)+(start)*(1-o1)*(y1)*(1-o2)*(y2);
   return eRThreeTwov;
 }
 
-function eRTwoThree(){
-  eRTwoThreev=(1-starter)*(1-ycp1)*(ycp2)*(ocp1)*(1-ocp1)+(1-starter)*(ycp1)*(ocp1)*(1-ocp1)*(1-ycp2)+(starter)*(ocp1)*(1-ocp1)*(1-ycp1)*(1-ycp2)+(starter)*(1-ocp1)*(ycp1)*(1-ocp2)*(1-ycp2)+(starter)*(1-ocp1)*(1-ycp1)*(ycp2)*(1-ocp2)+(1-starter)*(ycp1)*(1-ocp1)*(ycp2)*(1-ocp2);
+function eRTwoThree(start,o1,o2,y1,y2){
+  eRTwoThreev=(1-start)*(1-y1)*(y2)*(o1)*(1-o1)+(1-start)*(y1)*(o1)*(1-o1)*(1-y2)+(start)*(o1)*(1-o1)*(1-y1)*(1-y2)+(start)*(1-o1)*(y1)*(1-o2)*(1-y2)+(start)*(1-o1)*(1-y1)*(y2)*(1-o2)+(1-start)*(y1)*(1-o1)*(y2)*(1-o2);
   return eRTwoThreev;
 }
 
-function eROneThree(){
-  eROneThreev=(1-starter)*(ycp1)*(1-ocp1)*(1-ycp2)+(1-starter)*(1-ycp1)*(ycp2)*(1-ocp1)+(starter)*(1-ocp1)*(1-ycp1)*(1-ycp2);
+function eROneThree(start,o1,o2,y1,y2){
+  eROneThreev=(1-start)*(y1)*(1-o1)*(1-y2)+(1-start)*(1-y1)*(y2)*(1-o1)+(start)*(1-o1)*(1-y1)*(1-y2);
   return eROneThreev;
 }
 
-function eRZeroThree(){
-  eRZeroThreev=(1-starter)*(1-ycp1)*(1-ycp2);
+function eRZeroThree(start,o1,o2,y1,y2){
+  eRZeroThreev=(1-start)*(1-y1)*(1-y2);
   return eRZeroThreev;
 }
 
-function eRWin5(){
-  winChance=(eRThreeZero()+eRThreeOne()+eRThreeTwo());
+function eRWin5(start,o1,o2,y1,y2){
+  winChance=(eRThreeZero(start,o1,o2,y1,y2)+eRThreeOne(start,o1,o2,y1,y2)+eRThreeTwo(start,o1,o2,y1,y2));
   return winChance;
 }
 
-function eRLose5(){
-  loseChance=eRTwoThree()+eROneThree()+eRZeroThree();
+function eRLose5(start,o1,o2,y1,y2){
+  loseChance=eRTwoThree(start,o1,o2,y1,y2)+eROneThree(start,o1,o2,y1,y2)+eRZeroThree(start,o1,o2,y1,y2);
   return loseChance;
 }
 
-function yRWin5(){
-  winChance=(yRThreeZero()+yRThreeOne()+yRThreeTwo());
+function yRWin5(start,o1,o2,y1,y2){
+  winChance=(yRThreeZero(start,o1,o2,y1,y2)+yRThreeOne(start,o1,o2,y1,y2)+yRThreeTwo(start,o1,o2,y1,y2));
   return winChance;
 }
 
-function yRLose5(){
-  loseChance=yRTwoThree()+yROneThree()+yRZeroThree();
+function yRLose5(start,o1,o2,y1,y2){
+  loseChance=yRTwoThree(start,o1,o2,y1,y2)+yROneThree(start,o1,o2,y1,y2)+yRZeroThree(start,o1,o2,y1,y2);
   return loseChance;
 }
