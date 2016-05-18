c97b4d6f846d37bc98b11f27f94f2bdac7d6c0bb
schmooblidon <will_beats@hotmail.co.uk>
2016-05-18 18:41:34 +0100

    new header menu buttons

--- script.js
+++ script.js
@@ -40,24 +40,28 @@ var resizing = function() {
 			$(".button").css({"font-size":"12px","width":70}).children("p").css("margin","23px 0px");
 			$(".longbutton").css("width",90);
       $(".doublebutton").css("width",80).children("p").css("margin","16px 0px");
+      $(".doublelongbutton").css("width",90).children("p").css("margin","16px 0px");
 			$("#titlelogo").css({"width":280,"height":60,"margin-top":0});
 		}
 		else {
 			$(".button").css({"font-size":"16px","width":80}).children("p").css("margin","21px 0px");
 			$(".longbutton").css("width",100);
       $(".doublebutton").css("width",85).children("p").css("margin","12px 0px");
+      $(".doublelongbutton").css("width",100).children("p").css("margin","12px 0px");
 			$("#titlelogo").css({"width":280,"height":60,"margin-top":0});
 		}
 		if (windwidth < 1050){
 			$(".button").css({"font-size":"12px","width":65}).children("p").css("margin","23px 0px");
 			$(".longbutton").css("width",75);
-      $(".doublebutton").css("width",70).children("p").css("margin","16px 0px");;
+      $(".doublebutton").css("width",70).children("p").css("margin","16px 0px");
+      $(".doublelongbutton").css("width",75).children("p").css("margin","16px 0px");
 			$("#titlelogo").css({"width":200,"height":43,"margin-top":10});
 		}
 		if (windwidth < 905){
 			$(".button").css({"font-size":"10px","width":50}).children("p").css("margin","25px 0px");
 			$(".longbutton").css("width",60);
-      $(".doublebutton").css("width",55).children("p").css("margin","19px 0px");;
+      $(".doublebutton").css("width",55).children("p").css("margin","19px 0px");
+      $(".doublelongbutton").css("width",60).children("p").css("margin","19px 0px");
 			$("#titlelogo").css({"width":150,"height":32,"margin-top":16});
 		}
     $("#main").css("min-height",windheight-105+"px");
@@ -89,4 +93,9 @@ $(document).ready(function(){
 	setTimeout(resizing,500);
 	setTimeout(resizing,1000);
 	setTimeout(resizing,5000);
+  $("#appsButton").hover(function(){
+    $("#appsDropdown").show();
+  }, function(){
+    $("#appsDropdown").hide();
+  });
 });
